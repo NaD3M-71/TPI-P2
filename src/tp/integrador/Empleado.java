60d515813fc145a5241326cab25df996d720d40e
Giuliano Scaglioni <scaglioni96@gmail.com>
2025-11-11 12:42:12 -0300

    first commit

---
+++ src/tp/integrador/Empleado.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package tp.integrador;
+
+/**
+ *
+ * @author Giuliano Scaglioni
+ */
+public class Empleado {
+    private String dni; //PK
+    private String nombre;
+    private String apellido;
+    private String email;
+    private Boolean eliminado;
+    private java.time.LocalDate fechaIngreso;
+    private String area;
+    private Legajo legajo;
+    
+    // constructor
+    public Empleado(String dni, String nombre, String apellido, String email,
+                boolean eliminado, java.time.LocalDate fechaIngreso, String area, Legajo legajo) {
+    this.dni = dni;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.eliminado = eliminado;
+    this.fechaIngreso = fechaIngreso;
+    this.area = area;
+    this.legajo = legajo;
+}
+    
+    // Getters
+    public String getNombreCompleto(){
+        return nombre+", "+apellido ;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public boolean isEliminado() {
+    return eliminado;
+    }
+    public java.time.LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+    public String getArea() {
+        return area;
+    }
+    public Legajo getLegajo() {
+        return legajo;
+    }
+    
+    // setters
+    public void setLegajo(Legajo legajo){
+        this.legajo = legajo;
+    }
+    //crearEmpleado
+    public static Empleado crearEmpleado(
+        String dni,
+        String nombre,
+        String apellido,
+        String email,
+        String categoria,
+        Estado estado,
+        String observaciones,
+        String area
+
+    ){
+        java.time.LocalDate fechaIngreso = java.time.LocalDate.now();
+
+        Legajo legajo = new Legajo();
+        legajo.setNroLegajo(Legajo.generarNroLegajoUnico()); //TODO
+        legajo.setEliminado(false);
+        legajo.setCategoria(categoria);
+        legajo.setEstado(estado);
+        legajo.setFechaIngreso(fechaIngreso);
+        legajo.setObservaciones(observaciones);
+
+        return new Empleado(dni, nombre, apellido, email, false, fechaIngreso, area, legajo);
+    }
+
+
+    public void mostrarLegajo(Legajo legajo){
+        
+    }
+    
+    @Override
+    public String toString() {
+    return "Empleado{" +
+           ", dni='" + dni + '\'' +
+           ", nombre='" + nombre + '\'' +
+           ", apellido='" + apellido + '\'' +
+           ", email='" + email + '\'' +
+           ", area='" + area + '\'' +
+           ", fechaIngreso=" + fechaIngreso +
+           ", legajo=" + (legajo != null ? legajo.getNroLegajo() : "sin asignar") +
+           '}';
+    }
+
+}
---
+++ src/tp/integrador/Legajo.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package tp.integrador;
+
+/**
+ *
+ * @author Giuliano Scaglioni
+ */
+public class Legajo {
+    private String nroLegajo;
+    private Boolean eliminado;
+    private String categoria;
+    private Estado estado;
+    private java.time.LocalDate fechaAlta;
+    private String observaciones;
+    
+    // constructor
+    public Legajo() {}
+
+    public Legajo(Long id, String nroLegajo, Boolean eliminado, String categoria,
+                  Estado estado, java.time.LocalDate fechaAlta, String observaciones) {
+
+        this.nroLegajo = nroLegajo;
+        this.eliminado = eliminado;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.fechaAlta = fechaAlta;
+        this.observaciones = observaciones;
+    }
+    
+    //getters
+    public String getNroLegajo() { return nroLegajo; }
+    public Boolean getEliminado() { return eliminado; }
+    public String getCategoria() { return categoria; }
+    public Estado getEstado() { return estado; }
+    public java.time.LocalDate getFechaAlta() { return fechaAlta; }
+    public String getObservaciones() { return observaciones; }
+
+    
+    //settersLegajo
+    public void setNroLegajo(String nroLegajo) { this.nroLegajo = nroLegajo; }
+    public void setEliminado(boolean eliminado) { this.eliminado = eliminado; }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
+    public void setEstado(Estado estado) { this.estado = estado; }
+    public void setFechaIngreso(java.time.LocalDate fechaAlta) { this.fechaAlta = fechaAlta; }
+    public void setObservaciones(String observaciones) { this.observaciones = observaciones; }
+    
+    public static String generarNroLegajoUnico() {
+    return "LEG-" + java.util.UUID.randomUUID().toString();
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Legajo{" +
+               ", nroLegajo='" + nroLegajo + '\'' +
+               ", categoria='" + categoria + '\'' +
+               ", estado=" + estado +
+               ", fechaAlta=" + fechaAlta +
+               ", observaciones='" + observaciones + '\'' +
+               '}';
+    }
+
+}
